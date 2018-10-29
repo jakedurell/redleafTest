ac96194e381922413ae4120e1b4c9372b414ee9b
Jake D <jdurell@gmail.com>
2018-10-29 10:25:20 -0400

    got user login to show pulsing grey circle

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,9 @@
+import { UserLoginComponent } from './user-login/user-login.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'userLogin', pathMatch: 'full' },
-  {
-    path: 'userLogin',
-    loadChildren: './layouts/user-login/user-login.module#UserLoginModule'
-  },
+  { path: '', component: UserLoginComponent, pathMatch: 'full' },
   {
     path: 'dashboard',
     loadChildren: './layouts/dashboard/dashboard.module#DashboardModule'
