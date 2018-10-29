7aa7157c988a44a86be42345facd69efa8b1e75a
Jake D <jdurell@gmail.com>
2018-10-29 12:09:30 -0400

    kendo errors

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { UserLoginComponent } from './user-login/user-login.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { OfficesComponent } from './offices/offices.component';
 
 const routes: Routes = [
   { path: '', component: UserLoginComponent, pathMatch: 'full' },
+  { path: 'offices', component: OfficesComponent, pathMatch: 'full' },
   {
     path: 'dashboard',
     loadChildren: './layouts/dashboard/dashboard.module#DashboardModule'
--- src/app/offices/offices.component.ts
+++ src/app/offices/offices.component.ts
@@ -1,15 +1,37 @@
+import { MockOffices } from './../mock-data/offices';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { State } from '@progress/kendo-data-query';
+import {
+    GridDataResult,
+    DataStateChangeEvent
+} from '@progress/kendo-angular-grid';
 
-@Component({
+
+@Component ({
   selector: 'app-offices',
   templateUrl: './offices.component.html',
   styleUrls: ['./offices.component.scss']
 })
 export class OfficesComponent implements OnInit {
 
-  constructor() { }
+  public view: Observable<GridDataResult>;
+  public state: State = {
+      skip: 0,
+      take: 5
+  };
+
+  // constructor(private service: MockOffices) {
+  //     this.view = service;
+  //     this.service.query(this.state);
+  // }
+
+  // public dataStateChange(state: DataStateChangeEvent): void {
+  //     this.state = state;
+  //     this.service.query(state);
+  // }
 
   ngOnInit() {
+    console.log(MockOffices);
   }
-
 }
